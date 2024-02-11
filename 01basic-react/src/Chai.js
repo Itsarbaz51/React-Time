6f76ecd981bb4034094b7806d19dad9cae57dc98
arbaz khan <itsarbaz51@gmail.com>
2024-02-11 20:06:33 +0530

    basic react & create-react-app

---
+++ 01basic-react/src/Chai.js
@@ -0,0 +1,11 @@
+function Chai(){
+    return(
+        // Fragment consept <> </>
+        <>
+        <h1>Hello world!</h1>
+        <h2>Hyy react</h2>
+        </>
+    )
+}
+
+export default Chai
