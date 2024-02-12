837fb06c30ac7e45341fc789e29cd431351d3cfc
arbaz khan <itsarbaz51@gmail.com>
2024-02-12 23:42:20 +0530

    understanding hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(2)
+  
+  const addValue = ()=> {
+    setCounter(counter++)
+  }
+
+  const removeValue = () => {
+    setCounter(counter--)
+  }
+  
+
+  return(
+    <>
+
+    <h1>Lets Make it counter</h1>
+    <h2>Counter Vlaue is: {counter}</h2>
+    <br />
+    <button onClick={addValue}>Add Vlaue</button>
+    <br />
+    <br />
+    <button onClick={removeValue}>Remove Vlaue</button>
+
+    </>
+  )
+}
+
+export default App
